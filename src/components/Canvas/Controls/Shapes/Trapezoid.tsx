8f5b0f3705922d1cd9a798230f12322df88fdd3b
Kevin Fries <kevinsctfries@gmail.com>
2025-07-04 21:42:00 -0400

    Replace using CSS for shapes with SVGs instead

--- src/components/Canvas/Controls/Shapes/Trapezoid.tsx
+++ src/components/Canvas/Controls/Shapes/Trapezoid.tsx
@@ -1,22 +1,18 @@
 import { Handle, NodeProps, Position } from "reactflow";
 import styles from "./Shapes.module.scss";
 
-const handleStyle = { left: 10 };
+const inputA = { left: 25 };
+const inputB = { left: 75 };
 
-function Trapezoid({ data }: NodeProps) {
+function Trapezioid({ data }: NodeProps) {
   return (
     <div className={styles.trapezoid}>
       <Handle type="target" position={Position.Top} />
       <div>{data.label}</div>
-      <Handle
-        type="source"
-        position={Position.Bottom}
-        id="a"
-        style={handleStyle}
-      />
-      <Handle type="source" position={Position.Bottom} id="b" />
+      <Handle type="source" position={Position.Bottom} id="a" style={inputA} />
+      <Handle type="source" position={Position.Bottom} id="b" style={inputB} />
     </div>
   );
 }
 
-export default Trapezoid;
+export default Trapezioid;
--- src/components/Canvas/Controls/Shapes/Triangle.tsx
+++ src/components/Canvas/Controls/Shapes/Triangle.tsx
@@ -1,20 +1,16 @@
 import { Handle, NodeProps, Position } from "reactflow";
 import styles from "./Shapes.module.scss";
 
-const handleStyle = { left: 10 };
+const inputA = { left: 25 };
+const inputB = { left: 75 };
 
 function Triangle({ data }: NodeProps) {
   return (
     <div className={styles.triangle}>
       <Handle type="target" position={Position.Top} />
       <div>{data.label}</div>
-      <Handle
-        type="source"
-        position={Position.Bottom}
-        id="a"
-        style={handleStyle}
-      />
-      <Handle type="source" position={Position.Bottom} id="b" />
+      <Handle type="source" position={Position.Bottom} id="a" style={inputA} />
+      <Handle type="source" position={Position.Bottom} id="b" style={inputB} />
     </div>
   );
 }
