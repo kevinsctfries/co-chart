67151d6f958c31b3e3bdcec180dde02dda1c297a
Kevin Fries <kevinsctfries@gmail.com>
2025-07-05 00:30:03 -0400

    remove extra input from nodes

--- src/components/Canvas/Controls/Shapes/Rectangle.tsx
+++ src/components/Canvas/Controls/Shapes/Rectangle.tsx
@@ -1,16 +1,12 @@
 import { Handle, NodeProps, Position } from "reactflow";
 import styles from "./Shapes.module.scss";
 
-const inputA = { left: 25 };
-const inputB = { left: 75 };
-
 function Rectangle({ data }: NodeProps) {
   return (
     <div className={styles.rectangle}>
       <Handle type="target" position={Position.Top} />
       <div>{data.label}</div>
-      <Handle type="source" position={Position.Bottom} id="a" style={inputA} />
-      <Handle type="source" position={Position.Bottom} id="b" style={inputB} />
+      <Handle type="source" position={Position.Bottom} id="a" />
     </div>
   );
 }
--- src/components/Canvas/Controls/Shapes/Trapezoid.tsx
+++ src/components/Canvas/Controls/Shapes/Trapezoid.tsx
@@ -1,16 +1,12 @@
 import { Handle, NodeProps, Position } from "reactflow";
 import styles from "./Shapes.module.scss";
 
-const inputA = { left: 25 };
-const inputB = { left: 75 };
-
 function Trapezioid({ data }: NodeProps) {
   return (
     <div className={styles.trapezoid}>
       <Handle type="target" position={Position.Top} />
       <div>{data.label}</div>
-      <Handle type="source" position={Position.Bottom} id="a" style={inputA} />
-      <Handle type="source" position={Position.Bottom} id="b" style={inputB} />
+      <Handle type="source" position={Position.Bottom} id="a" />
     </div>
   );
 }
